79b5136710a96415db9745ac4781464eb66b52f3
jandy14_ <cbs3080@daum.net>
2016-10-12 14:39:24 +0900

    빗소리와 bgm추가

---
+++ Assets/Scripts/AllSceneBGM.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class AllSceneBGM : MonoBehaviour {
+
+	private static GameObject musicBox;
+	private static float timer;
+
+	public GameObject musicSource;
+	public float delay;
+	void Start ()
+	{
+		if(musicBox && SceneManager.GetActiveScene().name == "WasteOfLife")
+		{
+			Destroy(musicBox);
+		}
+	}
+	void Update ()
+	{
+		if (!musicBox && timer > delay && SceneManager.GetActiveScene().name != "WasteOfLife" && SceneManager.GetActiveScene().name != "Ending")
+		{
+			musicBox = Instantiate(musicSource);
+			DontDestroyOnLoad(musicBox);
+		}
+
+		timer += Time.deltaTime;
+		Debug.Log(timer);
+	}
+}
